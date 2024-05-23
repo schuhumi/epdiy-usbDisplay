701784773b78b88b420d00919ccdc8a46ad1ec57
Simon Schumann <simon.schumann@tum.de>
2024-05-23 20:07:30 +0200

    more bulletproof data cache preloading

--- main/mycache.cpp
+++ main/mycache.cpp
@@ -6,8 +6,13 @@
 bool dcache_preload_active = false;
 
 void My_Cache_Start_DCache_Preload(uint32_t start, uint32_t len, bool finish_previous_preload) {
-    // Start manual preload of a piece of external ram. If a previous preload is still ongoing, it is being
-    // stopped.
+    // Start a manual preload of a piece of external ram. The requested region is padded such that
+    // the preloading always works (expanded into blocks and one block at a minimum).
+    // If a previous preload is still ongoing, it depends on the finish_previous_preload variable
+    // what happens:
+    //  - true: the call blocks until the previous preload is finished. The task yields while waiting
+    //          to be able to get other independent things done.
+    //  - false: the previous preload is stopped prematurely, and the new one starts instead.
 
     if(dcache_preload_active) {
         if(Cache_DCache_Preload_Done()) {
@@ -18,19 +23,32 @@ void My_Cache_Start_DCache_Preload(uint32_t start, uint32_t len, bool finish_pre
                     taskYIELD();
                 }
             } else {
-                //ESP_LOGI("MyCache", "DCache preload is still active, stopping it first.");
                 Cache_End_DCache_Preload(0); // Do not start auto-preload
             }
             dcache_preload_active = false;
         }
     }
 
-    uint32_t block_start = start & DATA_CACHE_START_MASK;
-    uint32_t block_end = ((start + len) | DATA_CACHE_STOP_MASK) + 1;
+    uint32_t block_start = start & DATA_CACHE_BLOCK_BITS;
+    uint32_t block_end = start + len;  // This is exclusive, i.e. the first address we do not require
+    if((block_end & DATA_CACHE_OVERHANG_BITS) != 0) {
+        // the end of our desired area happens to not fit the block grid
+        // -> we need to include the block that our end overhangs into
+        block_end = (block_end | DATA_CACHE_OVERHANG_BITS) + 1;
+    }
+    if( ((block_start^block_end) & DATA_CACHE_BLOCK_BITS) != 0 ) {
+        // Our start and stop block are the same one, which makes a length of 0 blocks.
+        // We need to request 1 block at a minimum though.
+        block_end += CONFIG_ESP32S3_DATA_CACHE_LINE_SIZE;
+    }
+    // With a CONFIG_ESP32S3_DATA_CACHE_LINE==16 case as example, at this point we should have:
+    // block_start = 0b...aaaaa0000
+    // block_stop  = 0b...bbbbb0000
+    // where the block number indicated by the b bits is at least 1 larger than the one indicated by the a bits
     Cache_Start_DCache_Preload(
-        block_start,  // start address of the preload region
-        block_end - block_start,  // size of the preload region, should not exceed the size of DCache
-        0  // the preload order, 0 for positive, other for negative
+        block_start,                // start address of the preload region
+        block_end - block_start,    // size of the preload region, should not exceed the size of DCache
+        0                           // the preload order, 0 for positive, other for negative
     );
     dcache_preload_active = true;
 }
--- main/mycache.h
+++ main/mycache.h
@@ -3,15 +3,10 @@
 #include <rom/cache.h>
 #include <esp_types.h>  
 
-#if CONFIG_ESP32S3_DATA_CACHE_LINE_SIZE == 64
-    #define DATA_CACHE_START_MASK 0xFFFFFFC0
-    #define DATA_CACHE_STOP_MASK 0x3F
-#elif CONFIG_ESP32S3_DATA_CACHE_LINE_SIZE == 32
-    #define DATA_CACHE_START_MASK 0xFFFFFFE0
-    #define DATA_CACHE_STOP_MASK 0x1F
-#else // 16
-    #define DATA_CACHE_START_MASK 0xFFFFFFF0
-    #define DATA_CACHE_STOP_MASK 0x0F
-#endif
+// The CONFIG_ESP32S3_DATA_CACHE_LINE_SIZE is either 64, 32 or 16.
+// The bit mask that indicates the addresses inside a block can be calculated like this
+#define DATA_CACHE_OVERHANG_BITS ((uint32_t)CONFIG_ESP32S3_DATA_CACHE_LINE_SIZE-1)
+// The other bits denote the block number:
+#define DATA_CACHE_BLOCK_BITS (~DATA_CACHE_OVERHANG_BITS)
 
 void My_Cache_Start_DCache_Preload(uint32_t start, uint32_t len, bool finish_previous_preload);
